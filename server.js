a94cda7c68669be8e7d2d0c69dda7b12d55b8317
Dmitriy Grosh <d.grosh@wis.software>
2022-05-24 23:10:08 +0300

    add server, parcel, netlify and folders

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(`${__dirname}/dist`));
+
+console.log('==========>`${__dirname}/dist`', `${__dirname}/dist`)
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
