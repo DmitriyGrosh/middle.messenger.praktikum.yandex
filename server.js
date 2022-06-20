7c4247fda2af4fe38b902ba7322ccab74cb65b48
Dmitriy Grosh <d.grosh@wis.software>
2022-06-21 00:13:43 +0300

    fix server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 const app = express();
@@ -6,7 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
 
-console.log('==========>`${__dirname}/dist`', `${__dirname}/dist`)
+// eslint-disable-next-line no-template-curly-in-string
+console.log('==========>`${__dirname}/dist`', `${__dirname}/dist`);
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
